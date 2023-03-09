6f6fc323953b63c1f34fac1bfcd3664361b4b30d
Rui Armada <aepb.rui.98@gmail.com>
2023-03-09 14:05:28 +0000

    Started doing the loader function

--- include/scene.hpp
+++ include/scene.hpp
@@ -5,9 +5,15 @@
 
 class Scene {
 public:
+
+  static attrib_t attributes;
+  
+  static std::vector<shape_t> shapes;
+
+  static std::vector<material_t> materials;
   /**
    * Loads and creates a scene from a filename
    */
-  static std::optional<Scene> load(const std::string &filename);
+  static std::optional<Scene> load(const std::string &filename, const std::string mtlDir);
   ~Scene();
 };
--- src/scene.cpp
+++ src/scene.cpp
@@ -2,15 +2,63 @@
 
 #include <optional>
 #include <tiny_obj_loader.h>
+#include <iostream>
 
-std::optional<Scene> Scene::load(const std::string &filename) {
-  tinyobj::ObjReader myObjReader;
+using namespace tinyobj;
+
+static void info(const ObjReader obj) {
+
+  Scene::attributes = obj.GetAttrib();
+  Scene::shapes = obj.GetShapes();
+  Scene::materials = obj.GetMaterials();
+
+  std::cout << "# of vertices: " << (Scene::attributes.vertices.size() / 3) << std::endl;
+  std::cout << "# of normals: " << (Scene::attributes.normals.size() / 3) << std::endl;
+  std::cout << "# of textCoords: " << (Scene::attributes.texcoords.size() / 2) << std::endl;
+  std::cout << "# of shapes: " << Scene::shapes.size() << std::endl;
+  std::cout << "# of materials: " << Scene::materials.size() << std::endl;
+
+  auto it_shapes = Scene::shapes.begin();
+  for(; it_shapes != Scene::shapes.end(); it_shapes++) {
+    std::cout << "Processing shape " << it_shapes->name << std::endl;
+    auto it_vertex = it_shapes->mesh.indices.begin();
+    for(; it_vertex != it_shapes->mesh.indices.end(); ) {
+      for(int vert=0; vert<3; vert++) {
+        std::cout << it_vertex->vertex_index;
+        it_vertex++;
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    printf("There are %lu material indexes.\n", it_shapes->mesh.material_ids.size());
+  }
+  std::cout << "Shape " << it_shapes->name << " has been processed." << std::endl;
+}
+
+// https://github.com/canmom/rasteriser/blob/master/fileloader.cpp
+std::optional<Scene> Scene::load(const std::string &filename, const std::string mtlDir) {
+  
+  ObjReader myObjReader;
+  std::string name = "";
 
   if (!myObjReader.ParseFromFile(filename)) {
     return std::nullopt;
   } else {
     return std::make_optional<Scene>();
   }
+
+  Scene::attributes = myObjReader.GetAttrib();
+  Scene::shapes = myObjReader.GetShapes();
+  Scene::materials = myObjReader.GetMaterials();
+  std::string err;
+
+  auto it_shapes = Scene::shapes.begin();
+
+  if(name != it_shapes->name) { // The first time a new obj is loaded it shows you the info
+    info(myObjReader);
+    name = it_shapes->name;
+  }
+  bool success = LoadObj(&attributes, &shapes, &materials, &err, filename.c_str(), mtlDir.c_str(), true);
 }
 
 Scene::~Scene() {
