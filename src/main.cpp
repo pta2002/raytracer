4b63255d9450cdc6938dcd880ddf81bd1000fc13
Pedro Alves <pta2002@pta2002.com>
2023-03-10 19:16:20 +0000

    Specify scene name on error

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
-#include "fmt/core.h"
+#include <fmt/core.h>
 #include "scene.hpp"
-#include <stdio.h>
 
 int main(int argc, char **argv) {
   if (argc == 2) {
@@ -8,7 +7,7 @@ int main(int argc, char **argv) {
     if (scene.has_value()) {
       fmt::println("Loaded scene");
     } else {
-      fmt::println(stderr, "Failed to load scene");
+      fmt::println(stderr, "Failed to load scene {}", argv[1]);
       return -1;
     }
   } else {
