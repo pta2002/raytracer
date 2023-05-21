8e5479baa684fe1f39a6301d14f4adaf4a335548
Pedro Alves <pta2002@pta2002.com>
2023-05-21 17:38:41 +0100

    idk honestly

--- src/path_tracer_shader.cpp
+++ src/path_tracer_shader.cpp
@@ -1,5 +1,6 @@
 #include "path_tracer_shader.hpp"
 #include "fmt/core.h"
+#include "ray.hpp"
 #include <fmt/color.h>
 #include <glm/gtc/random.hpp>
 #define MAX_DEPTH 4
@@ -26,7 +27,7 @@ vec3 PathTracerShader::getColorInternal(const Intersection &intersection,
   float rnd_russian = ((float)rand()) / ((float)RAND_MAX);
   if (depth < MAX_DEPTH || rnd_russian < CONTINUE_P) {
     auto m = intersection.face->material;
-    vec3 lcolor = vec3(0,0,0);
+    vec3 lcolor = vec3(0, 0, 0);
     if (m->specular != vec3(0, 0, 0))
       lcolor += specularReflection(intersection, *m, depth + 1);
     if (m->diffuse != vec3(0, 0, 0)) {
@@ -48,8 +49,9 @@ vec3 PathTracerShader::diffuseReflection(const Intersection &isect,
   vec3 color{0, 0, 0};
   vec2 rand = glm::linearRand(vec2(0, 0), vec2(1, 1));
   float cosTheta = sqrtf(rand.y);
-  vec3 dAroundZ{cosf(2.f * M_PI * rand.x) * sqrtf(1.f - rand.y),
-                sinf(2.f * M_PI * rand.x) * sqrtf(1.f - rand.y), cosTheta};
+
+  vec3 dAroundZ{cosf(2.f * (float)M_PI * rand.x) * sqrtf(1.f - rand.y),
+                sinf(2.f * (float)M_PI * rand.x) * sqrtf(1.f - rand.y), cosTheta};
 
   vec3 rayZ = *isect.shadingNormal;
   vec3 rayX, rayY;
@@ -65,11 +67,14 @@ vec3 PathTracerShader::diffuseReflection(const Intersection &isect,
       dAroundZ.x * rayX.y + dAroundZ.y * rayY.y + dAroundZ.z * rayZ.y,
       dAroundZ.x * rayX.z + dAroundZ.y * rayY.z + dAroundZ.z * rayZ.z};
 
-  auto intersection = scene.castRay(rayOrigin, rayDir);
+  Ray diffuse(rayOrigin, rayDir);
+  diffuse.adjustOrigin(*isect.shadingNormal);
+
+  auto intersection = scene.castRay(diffuse.origin, diffuse.direction);
 
   if (!intersection.lightColor.has_value()) {
-    vec3 rColor = getColorInternal(intersection, depth);
-    color = (material.diffuse * rColor);
+    vec3 rColor = getColorInternal(intersection, depth+1);
+    color = (material.diffuse * rColor);// / (cosTheta / (float)M_PI);
   }
 
   return color;
@@ -96,15 +101,10 @@ vec3 PathTracerShader::directLighting(const Intersection &isect,
 
         if (cosL > 0) {
           // Light is NOT behind the primitive
-          vec3 shadowRayOrigin = *isect.pos;
+          Ray ray(*isect.pos, lDir);
+          ray.adjustOrigin(normal);
 
-          if (dot(lDir, normal) < 0) {
-            shadowRayOrigin -= 0.0001f * normal;
-          } else {
-            shadowRayOrigin += 0.0001f * normal;
-          }
-
-          if (scene.visibility(shadowRayOrigin, lDir, lDistance - 0.0001f))
+          if (scene.visibility(ray.origin, ray.direction, lDistance - 0.0001f))
             color += material.diffuse * pointLight.color * cosL;
         }
       }
@@ -125,16 +125,11 @@ vec3 PathTracerShader::directLighting(const Intersection &isect,
         float cosL_LA = dot(lightDir, areaLight.gem.planeNormal);
 
         if (cosL > 0 && cosL_LA <= 0) {
-          vec3 shadowRayOrigin = *isect.pos;
+          Ray ray(*isect.pos, lightDir);
+          ray.adjustOrigin(*isect.shadingNormal);
 
-          if (dot(lightDir, *isect.shadingNormal) < 0) {
-            shadowRayOrigin -= 0.0001f * *isect.shadingNormal;
-          } else {
-            shadowRayOrigin += 0.0001f * *isect.shadingNormal;
-          }
-
-          if (scene.visibility(shadowRayOrigin, lightDir,
-                               lightDistance - 0.001f))
+          if (scene.visibility(ray.origin, ray.direction,
+                               lightDistance - 0.0001f))
             color +=
                 (material.diffuse * areaLight.intensity * cosL) / areaLight.pdf;
         }
@@ -154,14 +149,17 @@ vec3 PathTracerShader::specularReflection(const Intersection &isect,
   float cosTheta;
 
   if (material.shininess < 1000) {
-    vec2 rand = glm::linearRand(vec2(0, 0), vec2(1, 1));
-    cosTheta = powf(rand.y, 1 / (material.shininess + 1));
-    float auxRand1 = powf(rand.y, 2 / (material.shininess + 1));
+    vec2 rnd = glm::linearRand(vec2(0, 0), vec2(1, 1));
+    cosTheta = powf(rnd.y, 1 / (material.shininess + 1));
+
+    float auxRand1 = powf(rnd.y, 2 / (material.shininess + 1));
     vec3 sAroundN{
-        cosf(2.f * (float)M_PI * rand.x) * sqrtf(1 - auxRand1),
-        sinf(2.f * (float)M_PI * rand.x) * sqrtf(1 - auxRand1),
+        cosf(2.f * (float)M_PI * rnd.x) * sqrtf(1 - auxRand1),
+        sinf(2.f * (float)M_PI * rnd.x) * sqrtf(1 - auxRand1),
         cosTheta,
     };
+
+    // The greater the shininess value, the more sAroundN will point up
 
     vec3 rayX, rayY;
     if (abs(rayDir.x) > abs(rayDir.y)) {
@@ -178,19 +176,16 @@ vec3 PathTracerShader::specularReflection(const Intersection &isect,
                   sAroundN.z * rayDir.z};
   }
 
-  // Ideal sepcular
-  vec3 rayOrigin = isect.pos.value();
+  // Ideal specular
+  Ray ray(*isect.pos, rayDir);
+  ray.adjustOrigin(*isect.shadingNormal);
 
-  if (dot(rayDir, *isect.shadingNormal) < 0) {
-    rayOrigin -= 0.0001f * *isect.shadingNormal;
-  } else {
-    rayOrigin += 0.0001f * *isect.shadingNormal;
-  }
-
-  auto intersection = scene.castRay(rayOrigin, rayDir);
+  auto intersection = scene.castRay(ray.origin, ray.direction);
   vec3 color = getColorInternal(intersection, depth);
 
-  if (material.shininess < 1000) {
+  if (material.shininess < 1000 && false) {
+    // Isto é suposto ajustar as cores para que tenham o peso probabilistico
+    // certo, mas na verdade acabam só a desaparecer... TODO
     float power = powf(cosTheta, material.shininess) / (2.f * (float)M_PI);
 
     // TODO: Há um bug aqui, isto está a ficar demasiado escuro
